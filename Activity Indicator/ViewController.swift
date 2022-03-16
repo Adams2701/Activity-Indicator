364bdae8cfef8a86b7dbe16b56825cc0f2d4f8df
Hamzat Adams <07063783524ade@gmail.com>
2022-03-16 11:52:51 +0100

    Initial Commit

---
+++ Activity Indicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Activity Indicator
+//
+//  Created by MAC on 16/03/2022.
+//  Copyright © 2022 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
